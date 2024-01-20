702de4bd582e6ba23ce5bed816865922b7da07ad
infinity12020 <dilshodov.abduvali@mail.ru>
2024-01-20 16:37:03 +0500

    первая фиксация

---
+++ script.js
@@ -0,0 +1,18 @@
+let doc =  document.querySelector("#first")   
+
+
+doc = Math.floor(Math.random() * (20 - 0  + 1) + 0);
+console.log(doc);
+
+
+let ment =  document.querySelectorAll(".p") 
+
+
+
+
+  ment  = Math.floor(Math.random() * (
+    ['Alex',
+      'John', 'Constantine', 'Tyler', 'Kabachok', 'Smith'])) 
+
+console.log(ment);
+
